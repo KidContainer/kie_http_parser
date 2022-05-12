88c215a186e02f8b17c3617ffa80baf0cfab179a
sunbo.kie <sunbo.kie@bytedance.com>
2022-05-12 05:09:16 +0000

    [ADD] add test for both libraries

---
+++ http1/test/http1_test.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+    std::cout<<"http1 test"<<std::endl;
+    return 0;
+}
---
+++ http2/test/http2_test.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+    std::cout<<"http2 test"<<std::endl;
+    return 0;
+}
