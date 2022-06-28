9f4864473f327582b5c6ac7ad1ec656023fa5c34
sunbo.kie <sunbo.kie@bytedance.com>
2022-06-28 02:37:59 +0000

    [MODIFY] use cmake install function rather than just copying the library and related headers and files.

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,5 @@
 from conan import ConanFile
-from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+from conan.tools.cmake import CMakeToolchain, CMake
 
 
 class KieHttpParserConan(ConanFile):
@@ -23,7 +23,7 @@ class KieHttpParserConan(ConanFile):
     default_options = {"shared": False, "fPIC": True, "with_http1": True, "with_http2": False}
 
     # Sources are located in the same place as this recipe, copy them to the recipe
-    exports_sources = "CMakeLists.txt", "http1/*", "http2/*"
+    exports_sources = "CMakeLists.txt", "http1/*", "http2/*", "LICENSE"
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -45,14 +45,8 @@ class KieHttpParserConan(ConanFile):
         cmake.build()
 
     def package(self):
-        self.copy("*.a", "lib", "", keep_path=False)
-        self.copy("*.lib", "lib", "", keep_path=False)
-        if self.options.with_http1:
-            self.copy("*.h", "include/http1", "http1/include")
-            self.copy("*.hpp", "include/http1", "http1/include")
-        if self.options.with_http2:
-            self.copy("*.h", "include/http2", "http2/include")
-            self.copy("*.hpp", "include/http2", "http2/include")
+        cmake = CMake(self)
+        cmake.install()
 
 
     def package_info(self):
